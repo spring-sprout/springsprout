8c710004e40650c400a3ca5aa3eb69c337f71422
June. <dosajun@gmail.com>
2010-11-16 13:13:24 +0900

    SSD-175 이미지 컨트롤러 변경

---
+++ src/springsprout/modules/study/board/imagePost/ImagePostRepository.java
@@ -0,0 +1,16 @@
+package springsprout.modules.study.board.imagePost;
+
+import java.util.List;
+
+import springsprout.common.dao.GenericDao;
+import springsprout.domain.study.board.ImagePost;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: whiteship
+ * Date: 2010. 10. 14
+ * Time: 오후 2:49:37
+ */
+public interface ImagePostRepository extends GenericDao<ImagePost> {
+	List<ImagePost> getRootPostList(int start, int limit);
+}
