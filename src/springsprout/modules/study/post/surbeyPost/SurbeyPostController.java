a79bcfe186c7386bb1fe69a29669b78ce6e66b79
June. <dosajun@gmail.com>
2010-12-08 21:53:25 +0900

    SSD-211 컨트롤러 약간 작성

--- src/springsprout/modules/study/post/surbeyPost/SurbeyPostController.java
+++ src/springsprout/modules/study/post/surbeyPost/SurbeyPostController.java
@@ -1,5 +1,12 @@
 package springsprout.modules.study.post.surbeyPost;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@RequestMapping("/study/{id}/post/surbeyPost")
+@SessionAttributes({"study"})
 public class SurbeyPostController {
 
 }
