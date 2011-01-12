c37c531639bc13ae2e7b34f7ccc145522b6e64a6
unknown <nhn@.(none)>
2011-01-12 11:14:07 +0900

    페이스북 퀴즈 - Student

---
+++ test/sandbox/stediousstudent/StudentTest.java
@@ -0,0 +1,44 @@
+package sandbox.stediousstudent;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * User: Whiteship
+ * Date: 11. 1. 10
+ * Time: 오후 2:12
+ */
+public class StudentTest {
+
+    @Test
+    public void sortWord(){
+        Student student = new Student();
+//        assertThat(student.study("facebook hacker cup for studious students"), is("cupfacebookforhackerstudentsstudious"));
+//        assertThat(student.study("k duz q rc lvraw"), is("duzklvrawqrc"));
+//        assertThat(student.study("mybea zdr yubx xe dyroiy"), is("dyroiymybeaxeyubxzdr"));
+//        assertThat(student.study("jibw ji jp bw jibw"), is("bwjibwjibwjijp"));
+//        assertThat(student.study("uiuy hopji li j dcyi"), is("dcyihopjijliuiuy"));
+        student.study("gm souyd fsrd bjnnuknqs rvncvkvssd gxfl wjmeagyob pahil nkfrcuhjh");
+        student.study("z dvqgfh wqx vnajabkqvs sdwkc dlhcnc ezrcvsc teje gzwwj");
+        student.study("uiuy hopji li j dcyi");
+        student.study("rnnabb ldk ndhn rnnaldk zeabbbb zeabb zea rnna bb");
+        student.study("mybea zdr yubx xe dyroiy");
+        student.study("hdfeax d s uxnnrzko nxpcu v njxqbnh aaqzeeb kxpkw");
+        student.study("fujv mzr kgukjmokvz schpxugnef p rjojpzbsro wpobp wl od");
+        student.study("ld r d lo rlo rlol l rd ix");
+        student.study("jg j uj ujnzdng nzdng nzdn e g ujnzdngj");
+        student.study("u ufmu ufmuqfy vmc ufm uqfy z vmcu qfy");
+        student.study("facebook hacker cup for studious students");
+        student.study("iccrmcrm mwp sil iccrmcrm ic odo iccrm crm odocrm");
+        student.study("ikjea pdizqbo cwvswrhe m fw yyxcoj ggwgyonra ep tbnoazzs");
+        student.study("nl jtdmdxu ux nlmnyzdxu mnyz jtdm nlmnyz dxu uxdxu");
+        student.study("o zt da wv brorejctww fu phnej ynrdkylwys ekggrmehcl");
+        student.study("dcn csmzj krnc vkcoume wvpva yqoexwujwp v cxepgptf xb");
+        student.study("wehfri kclm ri qgca gt qgcagt qgcagt wehf qgcagtqgca");
+        student.study("m xmnz mlk vlk lk iwkf lkiwkf vm v");
+        student.study("s minpax ax zit cyax zitax minp zitaxminp cy");
+        student.study("a yncoklkc ek yyfqebh je edzhujjc gpmb ktqimdtw opka");
+    }
+}
