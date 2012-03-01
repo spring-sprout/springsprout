755bec0c317bd9794bc650c42b8ba896f41905d2
isyoon <isyoonn@gmail.com>
2012-03-01 02:12:28 -0800

    낙서 추가 코드 정리.

--- web/resources/js/modules/graffiti.js
+++ web/resources/js/modules/graffiti.js
@@ -1,5 +1,6 @@
 ss.modules.graffiti = (function(ss){
   var suda,
+      itemTemplate,
       that = {
     init : function(){
       that.setEvent();
@@ -36,8 +37,10 @@ ss.modules.graffiti = (function(ss){
       e.preventDefault();
       var gBox = $('#g-writer-box'),
           gWriterImg = $('.writer-thumb');
-      if($.trim(gBox.val())){
-        suda.emit('message',{name:gWriterImg.data('mName'),avatar:gWriterImg.attr('src'),id:gWriterImg.data('mId'),msg:gBox.val()});
+          gVal = $.trim(gBox.val());
+      if(gVal){
+        gVal = gVal.replace(/\n/ig,' ');
+        suda.emit('message',{name:gWriterImg.data('mName'),avatar:gWriterImg.attr('src'),id:gWriterImg.data('mId'),msg:gVal});
         gBox.val('').focus();
       }
     },
@@ -47,30 +50,49 @@ ss.modules.graffiti = (function(ss){
       that.printSudaData();
     },
 
+    makeItem : function(data){
+      if(!itemTemplate){
+        itemTemplate =   '<li class="stream-item">' +
+                         '  <div class="writer-thumb-wrap">' +
+                         '    <a href="/member/${id}">' +
+                         '      <img class="writer-thumb" src="${avatar}" alt="${name}">' +
+                         '    </a>' +
+                         '  </div>' +
+                         '  <div class="writer-text">' +
+                         '    <div class="item-header"><strong>${name}</strong> <small class="item-time" title="${time}">on ${time}</small></div>' +
+                         '    <div class="item-content">${msg}' +
+                         '    </div>' +
+                         '  </div>' +
+                         '</li>';
+      }
+      var html = itemTemplate;
+      if(data.writtenDate){
+        data.time = that.covertDateString(data.writtenDate);
+      }
+      $.each(data, function(key, value){
+        html = html.replace(new RegExp("\\$\\{" + key + "\\}", "gi"), value);
+      });
+      return html;
+
+    },
+
     printSudaData : function(){
       var sItems = $('.stream-items');
       suda.on('message', function (data) {
         if(data){
-          var li =
-            '<li class="stream-item">' +
-              '<div class="writer-thumb-wrap">' +
-              '<a href="/member/' + data.id + '">' +
-              '<img class="writer-thumb" src="' + data.avatar + '" alt="' + data.name + '">' +
-              '</a>' +
-              '</div>' +
-              '<div class="writer-text">' +
-              '<div class="item-header"><strong>' + data.name + '</strong> <small class="item-time" title="2012.03.01 09:21:01">on 03.01 09:21:01</small></div>' +
-              '<div class="item-content">' +
-                data.msg +
-              '</div>' +
-              '</div>' +
-              '</li>';
-          sItems.prepend(li);
+          sItems.prepend(that.makeItem(data));
         }
       });
+    },
+
+    covertDateString : function(date){
+      var date = new Date(date||Date.now()).toString().split(" "),
+          month = ['month','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].indexOf(date[1]);
+      if(month < 10){
+        month = '0'+month;
+      }
+      return month+'.'+date[2]+' '+date[4];
     }
-
-
   };
   return that;
 })(ss);
